67a63375246f257f82fbf2357af129601d3c8d50
wjsoj <wjs@wjsphy.top>
2024-01-27 18:30:23 +0800

    fix about page background color

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default async function About() {
     <div className='flex flex-col py-10 px-4'>
       <h1 className='text-gradient text-5xl text-center font-extrabold'>About</h1>
       <Dashboard />
-      <div className="my-4 px-4 rounded-xl shadow-lg bg-gradient-to-br from-pink-400/10 to-slate-200">
+      <div className="my-4 px-4 rounded-xl shadow-lg bg-gradient-to-br from-pink-400/10 to-slate-200 dark:from-violet-700/40 dark:to-slate-700/30">
         <QandA />
       </div>
     </div>
