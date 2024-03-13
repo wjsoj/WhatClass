5a27c40634a2e4e5de8d4c112d3e4954661d7acb
wjsoj <wjs@wjsphy.top>
2024-03-13 17:59:47 +0800

    change select to switch toggle

--- src/components/RoomPage.tsx
+++ src/components/RoomPage.tsx
@@ -2,15 +2,7 @@ import { useState,useEffect } from "react"
 import ClassRoom from "@/components/ClassRoom"
 import EmptyClassroom from "./EmptyClassroom"
 import TableSkeleton from "@/components/TableSkeleton"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectLabel,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { Switch } from "@/components/ui/switch"
 import { Label } from "@/components/ui/label"
 import { getFloor } from "./options"
 
@@ -22,7 +14,7 @@ export default function RoomPage() {
   let [classTime, setClassTime] = useState("今天")
   let [classInfo, setClassInfo] = useState({} as any)
   let [Loading, setLoading] = useState(true)
-  let [isEmptyView, setIsEmptyView] = useState("true")
+  let [isEmptyView, setIsEmptyView] = useState(true)
 
   async function fetchClassroom(classroom: string,classTime: string) {
     setLoading(true)
@@ -41,7 +33,7 @@ export default function RoomPage() {
     fetchClassroom(classroom,classTime).then(res => setClassInfo(res))
   }, [classroom,classTime])
   useEffect(() => {
-    if(isEmptyView==="true") {
+    if(isEmptyView===true) {
       setClassTime("今天")
     }
   }, [isEmptyView])
@@ -66,21 +58,11 @@ export default function RoomPage() {
           <button key={index} className={`px-4 py-2 rounded-none ${classroom === item ? "bg-sky-300/60 dark:bg-sky-800/40 text-slaye-900 dark:text-white" : " bg-slate-200 dark:bg-slate-700/40 text-slate-600 dark:text-slate-400"}`} onClick={() => setClassroom(item)}>{item}</button>
         ))}
       </div>
-      <div className="flex flex-row space-x-6 justify-center items-center mx-10">
-        <Label className="ml-2 shrink-0 text-lg">视图：</Label>
-        <Select value={isEmptyView} onValueChange={(e)=>{setIsEmptyView(e)}}>
-          <SelectTrigger>
-            <SelectValue placeholder="选择视图模式" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="false">全部教室信息</SelectItem>
-              <SelectItem value="true">仅空闲教室</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+      <div className="flex flex-row space-x-2 justify-center items-center mx-10">
+        <Switch className="border border-slate-200 dark:border-slate-600" checked={isEmptyView} onCheckedChange={()=>{setIsEmptyView(!isEmptyView)}} />
+        <Label className="ml-2 shrink-0">仅空闲教室</Label>
       </div>
-      {isEmptyView === "false" ? <AllClassroom /> : (!Loading ? <EmptyClassroom classroom={classInfo.rows} floor={getFloor(classroom)} loading={false}/> : <EmptyClassroom classroom={classInfo.rows} floor={getFloor(classroom)} loading/>)}
+      {isEmptyView === false ? <AllClassroom /> : (!Loading ? <EmptyClassroom classroom={classInfo.rows} floor={getFloor(classroom)} loading={false}/> : <EmptyClassroom classroom={classInfo.rows} floor={getFloor(classroom)} loading/>)}
     </>
   )
 }
